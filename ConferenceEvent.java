6de12dba9b7d74e3648fb36df1f79d25bb4fd61b
217230233wangcheng <2875880196@qq.com>
2024-03-26 22:49:22 +0800

    https://github.com/217230233wangcheng/Inheritance.git

--- ConferenceEvent.java
+++ ConferenceEvent.java
@@ -1,56 +1,48 @@
-public class ConferenceEvent extends Event {
-    private boolean breakfastRequired ;
-    private double brefakfastCost ;
-    private boolean lunchRequired ;
-    private double lunchCost ;
-    private boolean dinnerRequired ;
-    private double dinnerCost ;
-    private double conferenceEventCost ;
+import com.sun.java.accessibility.util.EventID;
 
-    public ConferenceEvent(String eventID, String eventName, String eventLocation, String pointOfContact,  int totalParticipants, int totalEventDays , 
-    double brefakfastCost, double lunchCost, double dinnerCost ) {
-       super(eventID, eventName, eventLocation, pointOfContact, totalParticipants, totalEventDays, totalEventDays);
-       this.brefakfastCost = brefakfastCost;
-       this.lunchCost = lunchCost;
-       this.dinnerCost = dinnerCost;
-   } 
-
-   public double getbreakfastCost() {
-       return brefakfastCost;
-   }
-
-   public void setbreakfastCost(double brefakfastCost) {
-       this.brefakfastCost = brefakfastCost;
-   }
-
-   public double getlunchCost() {
-       return lunchCost;
-   }
-
-   public void setlunchCost(double lunchCost) {
-       this.lunchCost = lunchCost;
-   }
-
-   public double getdinnerCost() {
-       return dinnerCost;
-   }
-
-   @Override
-   public void calculateEventCost() {
-       conferenceEventCost = geteventCost() +((getbreakfastCost() + getlunchCost() + getdinnerCost())*gettotalParticipants()*gettotalEventDays());
-   }
+public class ConferenceEvent{
+    private String eventID;
+    private String eventName;
+    private String eventLocation;
+    private String eventPointOfContact;
+    private double eventCost;
+    private int totalParticipants;
+    private int totalEventDays;
+    private boolean breakfastRequired;
+    private double breakfastCost;
+    private boolean lunchRequired;
+    private double lunchCost;
+    private boolean dinnerRequired;
+    private double dinnerCost;
+    private CalculateEventCostClass calculateEventCostObject=new CalculateEventCostClass();
 
 
-   @Override
-   public String toString() {
-       return "Conference Event details :" + "\n" +
-              "Event ID: " + geteventID() + "\n" +
-              "Event Name: " + geteventName() + "\n" +
-              "Event Location: " + geteventLocation() + "\n" +
-             "Total Participants: " + gettotalParticipants() + "\n" +
-             "Total Conference Cost: " + conferenceEventCost;
-               
-           }
+    public ConferenceEvent(String eventID, String eventName, String eventLocation, String eventPointOfContact, double eventCost, int totalParticipants, int totalEventDays,boolean breakfastRequired, double breakfastCost, boolean lunchRequired, double lunchCost, boolean dinnerRequired, double dinnerCost) {
+        this.eventID = eventID;
+        this.eventName = eventName;
+        this.eventLocation = eventLocation;
+        this.eventPointOfContact = eventPointOfContact;
+        this.eventCost = eventCost;
+        this.totalParticipants = totalParticipants;
+        this.totalEventDays = totalEventDays;
+        this.breakfastRequired = breakfastRequired;
+        this.breakfastCost = breakfastCost;
+        this.lunchRequired = lunchRequired;
+        this.lunchCost = lunchCost;
+        this.dinnerRequired = dinnerRequired;
+        this.dinnerCost = dinnerCost;
+    }
 
+    public double calculateEventCost(){
+         return  eventCost = calculateEventCostObject.calculateEventCost() + (calculateEventCostObject.calculateEventCost() * 0.3);
+    }
+    public String toString() {
+        return "Conference Event details:"+'\n'+
+                "Event ID: "+ eventID +'\n'+
+                "Event Name: "+eventName+'\n'+
+                "Event Location: "+eventLocation+'\n'+
+                "Total Participants: "+totalParticipants+'\n'+
+                "Total Event Cost:"+eventCost+'\n';
+    }
 }
-   
+
